b184b860be92c0101fa08030a44197ebf6ef8359
yndGen <yandepgy8@gmail.comm>
2024-07-23 11:25:22 +0700

    perbaikan other service

--- frontend/src/component/OtherService.jsx
+++ frontend/src/component/OtherService.jsx
@@ -1,44 +1,94 @@
 import React from 'react'
+import { FaPhone } from "react-icons/fa6";
+import { CiMail } from "react-icons/ci";
+import { CiGlobe } from "react-icons/ci";
+import { FaCheck } from "react-icons/fa";
+
+
+
 
 const OtherService = () => {
   return (
-    <div className="bg-[#f7f3ee] font-playfair">
-    <div className="w-[1440px] mx-auto p-8">
-      <div className="text-center mb-8">
-        <h2 className="text-[#cca97b] text-lg">THE RUDRA HOTEL</h2>
-        <h1 className="text-4xl text-[#494949] italic">Rooms & Suites</h1>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-        {/* Standard Room */}
-        <div className="relative">
-          <img src="https://placehold.co/600x400" alt="Standard Room" className="w-full h-auto" />
-          <div className="absolute bottom-0 left-0 w-full bg-black bg-opacity-50 text-white text-center py-2">
-            Standard Room
+    <div className="max-w-7xl mx-auto py-16 px-4 flex flex-col lg:flex-row lg:space-x-8">
+      <div className="flex-1 mb-8 lg:mb-0">
+        <h3 className="text-coklat text-lg font-barlow px-24">Best Prices</h3>
+        <h2 className="text-6xl mb-4 font-island">Other Services</h2>
+        <p className="text-black mb-4 font-barlow">The best prices for your relaxing vacation. The utanlisen quam nesitibulum ac quame odion elementum sceiuse the aucan.</p>
+        <p className="text-black font-barlow mb-8">Orci varius natoque penatibus et magnis disney parturient monte nascete ridiculus mus nellen etesque habitant morbíne.</p>
+        <div className="space-y-4">
+          <div className="flex items-center text-coklat">
+            <FaPhone />
+            <p className='ml-1 text-black font-gilda'>+91 0009988767</p>
           </div>
-          <div className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-[#f7f3ee] text-[#c1b09c] py-1 px-3 text-xs font-semibold rotate-90">
-            $150/DAY
+          <div className="flex items-center text-coklat">
+            <CiMail />
+            <p className='ml-1 text-black font-gilda'>info@Rudrahotel.com</p>
           </div>
-        </div>
-        {/* Family Room */}
-        <div className="relative">
-          <img src="https://placehold.co/600x400" alt="Family Room" className="w-full h-auto" />
-          <div className="absolute bottom-0 left-0 w-full bg-black bg-opacity-50 text-white text-center py-2">
-            Family Room
-          </div>
-        </div>
-        {/* Double Room */}
-        <div className="relative">
-          <img src="https://placehold.co/600x400" alt="Double Room" className="w-full h-auto" />
-          <div className="absolute bottom-0 left-0 w-full bg-black bg-opacity-50 text-white text-center py-2">
-            Double Room
-          </div>
-          <div className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-[#f7f3ee] text-[#c1b09c] py-1 px-3 text-xs font-semibold rotate-90">
-            $57/NIGHT
+          <div className="flex items-center text-coklat">
+            <CiGlobe />
+            <p className='ml-1 text-black font-gilda'>www.rudra.com</p>
           </div>
         </div>
       </div>
+
+
+      <div className="flex-1 lg:flex lg:space-x-8">
+        <div className="bg-white shadow-lg mb-8 lg:mb-0 lg:flex-1 lg:w-2/3">
+          <img src="https://placehold.co/600x400" alt="Classic Balcony Room" className="w-full" />
+          <div className="p-6 bg-coklat">
+            <h3 className="font-gilda text-3xl font-bold text-white">Classic Balcony Room</h3>
+          </div>
+          <div className="p-6">
+            <p className="text-2xl text-coklat mb-4">
+              <span className="text-4xl font-gilda text-coklat">$50</span>
+              <span className="text-2xl font-gilda text-black">/month</span>
+            </p>
+            <ul className="text-gray-700 space-y-2">
+              <li className="flex font-gilda items-center">
+                <FaCheck className="mr-2  text-coklat" />
+                Hotel ut nisan the duru
+              </li>
+              <li className="flex font-gilda items-center">
+                <FaCheck className="mr-2 text-coklat" />
+                Orci miss natoque vasa ince
+              </li>
+              <li className="flex font-gilda items-center">
+                <FaCheck className="mr-2 text-coklat" />
+                Clean sorem ipsum morbin
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white shadow-lg rounded-lg lg:flex-1">
+        <img src="https://placehold.co/600x400" alt="Room cleaning" className="w-full" />
+        <div className="p-6 bg-coklat">
+          <h3 className="font-gilda text-3xl font-bold text-white">Room Cleaning</h3>
+        </div>
+        <div className="p-6">
+          <p className="text-2xl text-coklat mb-4">
+            <span className="text-4xl font-gilda text-coklat">$240</span>
+            <span className="text-2xl font-gilda text-black">/month</span>
+          </p>
+          <ul className="text-gray-700 space-y-2">
+              <li className="flex font-gilda items-center">
+                <FaCheck className="mr-2  text-coklat" />
+                Hotel ut nisan the duru
+              </li>
+              <li className="flex font-gilda items-center">
+                <FaCheck className="mr-2 text-coklat" />
+                Orci miss natoque vasa ince
+              </li>
+              <li className="flex font-gilda items-center">
+                <FaCheck className="mr-2 text-coklat" />
+                Clean sorem ipsum morbin
+              </li>
+            </ul>
+        </div>
+      </div>
+      
     </div>
-  </div>
   )
 }
 
