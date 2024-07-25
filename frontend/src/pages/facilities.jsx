4d0aa212390d10913a1568b1ccfe386745e1531a
yndGen <yandepgy8@gmail.comm>
2024-07-25 15:19:43 +0700

    nambah beberapa sama template france

--- frontend/src/pages/facilities.jsx
+++ frontend/src/pages/facilities.jsx
@@ -4,23 +4,18 @@ import Template1 from '../component/Template1';
 import Navbar from '../component/navbar/navbar';
 import Footer from '../component/footer/footer';
 import Img1 from '../assets/image/IMG-20240618-WA0006.jpg';
+import Hotelfacilities from '../component/Hotelfacilities';
 
 const  Facilities = () => {
   const [backgroundImage] = useState(Img1);
-  const [subtitle] = useState("A EXPERIENCE FOR THE SENSE");
-  const [title] = useState("Accommodation");
-  const [paragraphs] = useState([
-    "Accommodations at Casa Evaliza comprise an expansive living & dining room, an air-conditioned media room, and four air-conditioned bedrooms complete with ensuite bathrooms.",
-    "The bedrooms are situated in a two-storey building, which stands adjacent to the single storey pavilion that houses the living and dining areas. Each bedroom is equipped a satellite-channel TV and DVD player, and a safe deposit box, while WiFi Internet is accessible throughout the property.",
-  ]);
   const [subheadingText] = useState("Luxury Hotel");
-  const [headingText] = useState("Accommodation");
+  const [headingText] = useState("Facilities");
 
   return (
     <div>
       <Navbar/>
       <TemplateImg backgroundImage={backgroundImage} subheadingText={subheadingText} headingText={headingText} />
-      <Template1 subtitle={subtitle} title={title} paragraphs={paragraphs} />
+      <Hotelfacilities/>
       <Footer/>
     </div>
   );
