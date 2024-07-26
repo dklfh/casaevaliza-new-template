937a4f08eecbf43adc916b2419bc75a4f5249c9d
yndGen <yandepgy8@gmail.comm>
2024-07-26 13:52:31 +0700

    nambah image

--- frontend/src/component/Gallery1.jsx
+++ frontend/src/component/Gallery1.jsx
@@ -1,52 +1,21 @@
-import React from 'react'
-const Gallery1 = () => {
-  return (
-    <div class="columns-1 md:columns-2 lg:columns-4 gap-4 space-y-4 p-8 xl:mt-0 mt-20">
-      <div class="h-min w-full">
-        <img data-modal-target="modal" class="object-cover cursor-pointer" src="image/casa-vila-1.jpg" alt="Img 1"
-          id="img1" />
-      </div>
-      <div class="h-min w-full">
-        <img class="object-cover" data-modal-target="modal" src="" />
-      </div>
-      <div class="h-min w-full">
-        <img class="object-cover" data-modal-target="modal" src="image/casa-vila-7.jpg" />
-      </div>
-      <div class="h-min w-full">
-        <img class="object-cover" data-modal-target="modal" src="image/casa-vila-3.jpg" />
-      </div>
-      <div class="h-min w-full">
-        <img class="object-cover" src="image/casa-vila-6.jpg" data-modal-target="modal" />
-      </div>
-      <div class="h-min w-full">
-        <img class="object-cover" src="image/casa-vila-5.jpg" data-modal-target="modal" />
-      </div>
-      <div class="h-min w-full">
-        <img class="object-cover" src="image/casa-vila-8.jpg" data-modal-target="modal" />
-      </div>
-      <div class="h-min w-full">
-        <img class="object-cover" src="image/casa-vila-9.jpg" data-modal-target="modal" />
-      </div>
-      <div class="h-min w-full">
-        <img class="object-cover" src="image/casa-vila-10.jpg" data-modal-target="modal" />
-      </div>
-      <div class="h-min w-full">
-        <img class="object-cover" src="image/casa-vila-14.jpg" data-modal-target="modal" />
-      </div>
-      <div class="h-min w-full">
-        <img class="object-cover" src="image/casa-vila-15.jpg" data-modal-target="modal" />
-      </div>
-      <div class="h-min w-full">
-        <img class="object-cover" src="image/casa-vila-16.jpg" data-modal-target="modal" />
-      </div>
-      <div class="h-min w-full">
-        <img class="object-cover" src="image/casa-vila-21.jpg" data-modal-target="modal" />
-      </div>
-      <div class="h-min w-full">
-        <img class="object-cover" src="image/casa-vila-20.jpg" data-modal-target="modal" />
-      </div>
-    </div>
-  )
-}
+import React from 'react';
 
-export default Gallery1
+const Gallery1 = ({ images }) => {
+  return (
+    <section className='md:m-32 my-10 mx-5'>
+      <div class="text-center">
+        <h3 class="text-[15px] font-barlowcondensed tracking-widest text-coklat">IMAGES</h3>
+        <h1 class="text-[45px] font-island text-gray-700">Image Gallery</h1>
+      </div>
+      <div className="columns-1 md:columns-2 lg:columns-4 gap-4 space-y-4 p-8 xl:mt-0 mt-1">
+        {images.map((image, index) => (
+          <div key={index} className="h-min w-full">
+            <img data-modal-target="modal" className="object-cover cursor-pointer" src={image} alt={`Img ${index + 1}`} />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Gallery1;
--- frontend/src/pages/gallery.jsx
+++ frontend/src/pages/gallery.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
-import Navbar from '../component/navbar/navbar'
-import Footer from '../component/footer/footer'
+import React, { useState } from 'react';
+import Navbar from '../component/navbar/navbar';
+import Footer from '../component/footer/footer';
+import Gallery1 from '../component/Gallery1';
 
-function Gallery() {
+// Import images
+import Casa1 from '../assets/image/casa-vila-1.jpg';
+import Casa2 from '../assets/image/casa-vila-2.jpg';
+import Casa3 from '../assets/image/casa-vila-3.jpg';
+import Casa5 from '../assets/image/casa-vila-5.jpg';
+import Casa6 from '../assets/image/casa-vila-6.jpg';
+import Casa7 from '../assets/image/casa-vila-7.jpg';
+import Casa8 from '../assets/image/casa-vila-8.jpg';
+import Casa9 from '../assets/image/casa-vila-9.jpg';
+import Casa10 from '../assets/image/casa-vila-10.jpg';
+import Casa12 from '../assets/image/casa-vila-12.jpg';
+import Casa14 from '../assets/image/casa-vila-14.jpg';
+import Casa15 from '../assets/image/casa-vila-15.jpg';
+import Casa16 from '../assets/image/casa-vila-16.jpg';
+import Casa17 from '../assets/image/casa-vila-17.jpg';
+import Casa20 from '../assets/image/casa-vila-20.jpg';
+import TemplateImg from '../component/TemplateImg';
+import Img1 from '../assets/image/galleryhead.jpg';
+
+const Gallery = () => {
+  const images = [
+    Casa1, Casa2, Casa3, Casa5, Casa6, Casa7, Casa8,
+    Casa9, Casa10, Casa12, Casa14, Casa15, Casa16, Casa17, Casa20
+  ];
+  const [backgroundImage] = useState(Img1);
+  const [subheadingText] = useState("Images");
+  const [headingText] = useState("Image Gallery");
+
   return (
     <div>
       <Navbar/>
+      <TemplateImg backgroundImage={backgroundImage} subheadingText={subheadingText} headingText={headingText} />
+      <Gallery1 images={images} />
       <Footer/>
     </div>
-  )
+  );
 }
 
-export default Gallery
+export default Gallery;
